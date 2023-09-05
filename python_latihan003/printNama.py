4e1d0a7a1663554a38803f9ca014881c7d40c502
Sufiah27 <18ddt21f1027@student.ptss.edu.my>
2023-09-05 12:56:42 +0800

    file baru

---
+++ python_latihan003/printNama.py
@@ -0,0 +1,9 @@
+def print_intro():
+    pelajar = {"Nama" : "SUFIAH", "NoMatrik" : "18DDT21F1027"}
+
+    print(pelajar["Nama"])
+    print(pelajar["NoMatrik"])
+
+
+if __name__ == '__main__':
+    print_intro()
